363772cefbb92bd4a7f5f5cdc6e0798a121c0d51
Pratik Panchak <pratik.panchal@indianic.com>
2017-02-24 14:44:25 +0530

    initial Commit By Patrick

---
+++ PratikPodsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PratikPodsDemo
+//
+//  Created by indianic on 24/02/17.
+//  Copyright © 2017 indianic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PratikPodsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PratikPodsDemo
+//
+//  Created by indianic on 24/02/17.
+//  Copyright © 2017 indianic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
